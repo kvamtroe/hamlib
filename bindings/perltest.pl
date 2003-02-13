66f6ba04becb478ec984474e64d9d6e6454a70d3
(no author) <(no author)>
2003-02-13 03:08:01 +0000

    This commit was manufactured by cvs2svn to create branch 'branch_ts2k'.
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/branches/branch_ts2k@1371 7ae35d74-ebe9-4afe-98af-79ac388436b8

---
+++ bindings/perltest.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+# make sure the module is there, hint: "ln -s .libs/hamlibperl.so hamlib.so"
+use hamlib;
+
+print "Version: $hamlib::hamlib_version\n";
+print "FM: $hamlib::RIG_MODE_FM\n";
+
+hamlib::rig_set_debug($hamlib::RIG_DEBUG_TRACE);
+
+$rig = new hamlib::Rig($hamlib::RIG_MODEL_DUMMY);
+$rig->open();
+
+# 1073741944 is token value for "itu_region"
+$rpath = $rig->get_conf("rig_pathname");
+$region = $rig->get_conf(1073741944);
+print "get_conf: path=\"$rpath\", ITU region=$region\n";
+
+
+$rig->set_freq(12000000, $hamlib::RIG_VFO_A);
+
+$f = $rig->get_freq();
+print "freq: $f\n";
+
+($mode, $width) = $rig->get_mode();
+
+print "mode: $mode, width: $width\n";
+
+print "ITU region: $rig->{state}->{itu_region}\n";
+print "Copyright: $rig->{caps}->{copyright}\n";
+$inf = $rig->get_info();
+
+print "get_info: $inf\n";
+
+$rig->set_level("VOX", 1);
+$lvl = $rig->get_level_i("VOX");
+print "level: $lvl\n";
+$rig->set_level($hamlib::RIG_LEVEL_VOX, 5);
+$lvl = $rig->get_level_i($hamlib::RIG_LEVEL_VOX);
+print "level: $lvl\n";
+
+
+
+$chan = new hamlib::Chan($hamlib::RIG_VFO_A);
+
+$rig->get_channel($chan);
+print "get_channel status: $rig->{error_status}\n";
+
+print "VFO: $chan->{vfo}, $chan->{freq}\n";
+
+$rig->close();
+
+# TODO:
+($long1, $lat1) = hamlib::locator2longlat("IN98EC");
+($long2, $lat2) = hamlib::locator2longlat("DM33DX");
+$loc1 = hamlib::longlat2locator($long1, $lat1);
+$loc2 = hamlib::longlat2locator($long2, $lat2);
+print "Loc1: $loc1\n";
+print "Loc2: $loc2\n";
+
+($dist, $az) = hamlib::qrb($long1, $lat1, $long2, $lat2);
+$longpath = hamlib::distance_long_path($dist);
+print "Distance: $dist km, long path: $longpath\n";
+($deg, $min, $sec) = hamlib::dec2dms($az);
+$az2 = hamlib::dms2dec($deg, $min, $sec);
+print "Bearing: $az, $deg° $min' $sec\", recoded: $az2\n"
+
+
---
+++ rotorez/rotorez.h
@@ -0,0 +1,56 @@
+/*
+ * Hamlib backend library for the DCU rotor command set.
+ *
+ * rotorez.h - (C) Nate Bargmann 2003 (n0nb at arrl.net)
+ *
+ * This shared library provides an API for communicating
+ * via serial interface to a Hy-Gain or Yaesu rotor using
+ * the Idiom Press Rotor-EZ or RotorCard interface.  It also
+ * supports the Hy-Gain DCU-1.
+ *
+ *
+ *    $Id: rotorez.h,v 1.3 2003-02-13 03:08:00 n0nb Exp $
+ *
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ */
+
+#ifndef _ROT_ROTOREZ_H
+#define _ROT_ROTOREZ_H 1
+
+#define AZ_READ_LEN 4
+
+extern const struct rot_caps rotorez_rot_caps;
+extern const struct rot_caps rotorcard_rot_caps;
+extern const struct rot_caps dcu_rot_caps;
+
+extern BACKEND_EXPORT(int) initrots_rotorez(void *be_handle);
+
+/* 
+ * API local implementation
+ *
+ */
+
+static int rotorez_rot_init(ROT *rot);
+static int rotorez_rot_cleanup(ROT *rot);
+
+static int rotorez_rot_set_position(ROT *rot, azimuth_t azimuth, elevation_t elevation);
+static int rotorez_rot_get_position(ROT *rot, azimuth_t *azimuth, elevation_t *elevation);
+
+static int rotorez_rot_stop(ROT *rot);
+
+#endif  /* _ROT_ROTOREZ_H */
+
